853c96a7bf8e74018e686fdf1cd53a63e6e4db40
Ivan Harbatsenka <1995van95@gmail.com>
2022-11-29 17:34:57 +0300

    Добавил программу для решения

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] mass;
+Console.WriteLine("Введите массив строк, используя пробел как разделитель между словами или числами");
+string massstr = Console.ReadLine();
+mass = massstr.Split(' ');
+var result = new string[mass .Length];
+var realSize = 0;
+foreach (var value in mass )
+{
+    if (value.Length <= 3)
+         {
+            result[realSize] = value;
+            realSize++;
+         }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
